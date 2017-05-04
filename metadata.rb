53ae769a529afed77c8d99f5634f6b59dacb7b59
Chase Bolt <chase.bolt@gmail.com>
2017-05-04 14:16:53 -0700

    removing yum dependency

--- metadata.rb
+++ metadata.rb
@@ -3,12 +3,10 @@ maintainer 'Chase Bolt'
 maintainer_email 'chase.bolt@gmail.com'
 license 'Apache 2.0'
 description 'Installs and configures the Duo Security yum repository'
-version '0.1.6'
+version '0.1.7'
 
 source_url 'https://github.com/chasebolt/chef-yum-duosecurity'
 issues_url 'https://github.com/chasebolt/chef-yum-duosecurity/issues'
-
-depends 'yum', '~> 3.2'
 
 supports 'amazon'
 supports 'centos'
